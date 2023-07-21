eb975c421bfc0cf3773e0e3aa15c03d3f16f3bbb
ksripathy <ksripathy@icloud.com>
2023-07-21 17:31:15 +0200

    Rotor class definition

---
+++ a1/src/rotor.py
@@ -0,0 +1,15 @@
+class rotor:
+    
+    def __init__(self, rotorDia, rotorBlades, fstreamVelc, tipSpeedRatio, bladePitch, bladeTwistDist, bladeChordDist):
+        
+        self.rotorDia = rotorDia
+        self.rotorBlades = rotorBlades
+        self.fstreamVelc = fstreamVelc
+        self.tipSpeedRatio = tipSpeedRatio
+        self.bladePitch = bladePitch
+        self.bladeTwistDist = bladeTwistDist
+        self.bladeChordDist = bladeChordDist
+        
+        
+        
+        
