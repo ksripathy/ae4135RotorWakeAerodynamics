7d6a01602a6d35866976b5c1670d983de5d3e949
ksripathy <ksripathy@icloud.com>
2023-07-31 19:33:17 +0200

    Implemented optzFunction with just axialInd arg

--- a1/src/miscTools.py
+++ a1/src/miscTools.py
@@ -6,7 +6,22 @@ def movingAverage(a, n=2):
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
-def optzFunction(optzArgs, BEMObject, annulusID):
+def optzFunction(optzArg, BEMObject, annulusID):
+    
+    #Since optimizer chooses the input argument randomly, azimuthal induction needs to be adjusted accordingly apriori in order to represent the inflow accurately
+    BEMObject.annulusAxialInd[annulusID] = optzArg
+    BEMObject.annulusAzimInd[annulusID] = BEMObject.annulusBladeAzimLoad2D[annulusID] / (2 * BEMObject.fstreamRho * (2 * np.pi* BEMObject.rotor.annulusLoc[annulusID] * BEMObject.rotor.rotorRadius) * BEMObject.fstreamVelc**2 * (1 - BEMObject.annulusAxialIndCorrected[annulusID]) * BEMObject.tsr * BEMObject.rotor.annulusLoc[annulusID])
+    
+    BEMObject.indToBladeLoadSingleDOF(annulusID)
+    BEMObject.momLoadToIndSingleDOF(annulusID)
+    
+    axialIndError = BEMObject.annulusAxialInd[annulusID] - BEMObject.annulusAxialIndCorrected[annulusID]
+    
+    iterationError = np.absolute(axialIndError)
+    
+    return iterationError
+
+def optzFunction2Args(optzArgs, BEMObject, annulusID):
     
     BEMObject.annulusAxialInd[annulusID] = optzArgs[0]
     BEMObject.annulusAzimInd[annulusID] = optzArgs[1]
