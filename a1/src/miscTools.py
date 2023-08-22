ea5165453a535a2628f4462196849451db0a2f50
ksripathy <ksripathy@icloud.com>
2023-08-22 15:54:10 +0200

    Implemented animVortUpdate support function

--- a1/src/miscTools.py
+++ a1/src/miscTools.py
@@ -18,3 +18,42 @@ def attributeArray(objectArray, attribute, args=None):
         
         return np.array([getattr(obj, attribute)(*args) for obj in objectArray])
     
+#Update function for vortRing animation
+def animVortUpdate(frame, *fargs):
+        
+        plotObj = fargs[0]
+        
+        funcReturnList = []
+        
+        for j in range(plotObj.VMObj.mesh.rotor.bladeQuantity):
+            
+            bladeObj = getattr(plotObj.VMObj.mesh, f"annulusBlade{j}")
+            
+            #Retreive coordinate locations to plot
+            QChordPos = attributeArray(bladeObj.QChordHistory[frame], 'controlPointLoc')
+            rootVortPos = attributeArray(bladeObj.rootVortHistory[frame], 'controlPointLoc')
+            tipVortPos = tipVortPos = attributeArray(bladeObj.tipVortHistory[frame], 'controlPointLoc')
+            
+            plotObj.QChordLines[j].set_xdata(QChordPos[:,0])
+            plotObj.QChordLines[j].set_ydata(QChordPos[:,1])
+            plotObj.QChordLines[j].set_3d_properties(QChordPos[:,2])
+            funcReturnList.append(plotObj.QChordLines[j])
+            
+            plotObj.rootVortLines[j].set_xdata(rootVortPos[:,0])
+            plotObj.rootVortLines[j].set_ydata(rootVortPos[:,1])
+            plotObj.rootVortLines[j].set_3d_properties(rootVortPos[:,2])
+            funcReturnList.append(plotObj.rootVortLines[j])
+            
+            plotObj.tipVortLines[j].set_xdata(tipVortPos[:,0])
+            plotObj.tipVortLines[j].set_ydata(tipVortPos[:,1])
+            plotObj.tipVortLines[j].set_3d_properties(tipVortPos[:,2])
+            funcReturnList.append(plotObj.tipVortLines[j])
+            
+        return tuple(funcReturnList)
+            
+            
+            
+            
+            
+    
+        
